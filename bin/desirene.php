be7f3eaff059b7b48c7ffdc1a3b63ff7b95e10f6
Luigi Pressello <luigi@pressello.com>
2016-11-02 11:24:46 +0100

    Modified desirene.php to drop Propel native commands

--- bin/desirene.php
+++ bin/desirene.php
@@ -16,35 +16,11 @@ $finder->files()->name('*.php')->in(__DIR__.'/../lib/Desirene/Command')->depth(0
 $ns = '\\Desirene\\Command\\';
 
 foreach ($finder as $file) {
-    $r  = new \ReflectionClass($ns.$file->getBasename('.php'));
-    if ($r->isSubclassOf('Symfony\\Component\\Console\\Command\\Command') && !$r->isAbstract()) {
-        $cmd = $r->newInstance();
-        $app->add($cmd);
-    }
-}
-
-$finder = new Finder();
-$finder->files()->name('*.php')->in(__DIR__.'/../lib/vendor/propel/propel/src/Propel/Generator/Command')->depth(0);
-
-$ns = '\\Propel\\Generator\\Command\\';
-
-foreach ($finder as $file) {
-    $r  = new \ReflectionClass($ns.$file->getBasename('.php'));
-    if ($r->isSubclassOf('Symfony\\Component\\Console\\Command\\Command') && !$r->isAbstract()) {
-        $cmd = $r->newInstance();
-        $cmd->setName(sprintf("propel:%s", str_replace(":", "-", $cmd->getName())));
-        $options = $cmd->getDefinition()->getOptions();
-        if(isset($options['config-dir']))
-        {
-          $options['config-dir']->setDefault('../config');
-          if(isset($options['output-file']))
-          {
-            $options['output-file']->setDefault('databases.php');
-          }
-          $cmd->getDefinition()->setOptions($options);
-        }
-        $app->add($cmd);
-    }
+  $r  = new \ReflectionClass($ns.$file->getBasename('.php'));
+  if ($r->isSubclassOf('Symfony\\Component\\Console\\Command\\Command') && !$r->isAbstract()) {
+    $cmd = $r->newInstance();
+    $app->add($cmd);
+  }
 }
 
 $app->run();
