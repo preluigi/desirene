2f5f05ce6dcc313097fe2f32294e15b47a1ac1ff
Luigi Pressello <luigi@pressello.com>
2016-11-08 21:05:25 +0100

    Added CORS headers

--- config/dependencies.php
+++ config/dependencies.php
@@ -1,2 +1,13 @@
 <?php
 $ci = $app->getContainer();
+
+$app->add(function($request, $response, $next)
+  {
+    return $next($request, $response)
+    ->withHeader('Access-Control-Allow-Origin', '*')
+    ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization, content-type')
+    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+    ->withHeader('Access-Control-Allow-Credentials', 'true')
+    ->withHeader('Access-Control-Max-Age', '86400');
+  }
+);
