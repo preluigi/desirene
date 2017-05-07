2b3887bb829e5c9967cb909e6943b4ec19f9a2e7
Luigi Pressello <luigi@pressello.com>
2017-05-07 13:19:20 +0200

    Added environment management and custom Command class support

--- bin/desirene.php
+++ bin/desirene.php
@@ -1,6 +1,8 @@
 <?php
 define("ROOT_DIR",realpath(__DIR__ . '/../'));
-require_once __DIR__.'/../lib/vendor/autoload.php';
+require_once ROOT_DIR . '/lib/vendor/autoload.php';
+define("ENV", require ROOT_DIR . '/config/env.php');
+require ROOT_DIR . '/lib/Desirene/config/databases.php';
 
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Console\Input\ArgvInput;
@@ -15,6 +17,32 @@ $finder = new Finder();
 $finder->files()->name('*.php')->in(__DIR__.'/../lib/Desirene/Command')->depth(0);
 
 $ns = '\\Desirene\\Command\\';
+
+foreach ($finder as $file) {
+  $r  = new \ReflectionClass($ns.$file->getBasename('.php'));
+  if ($r->isSubclassOf('Symfony\\Component\\Console\\Command\\Command') && !$r->isAbstract()) {
+    $cmd = $r->newInstance();
+    $app->add($cmd);
+  }
+}
+
+$finder = new Finder();
+$finder->files()->name('*.php')->in(__DIR__.'/../lib/Command')->depth(0);
+
+$ns = '\\Command\\';
+
+foreach ($finder as $file) {
+  $r  = new \ReflectionClass($ns.$file->getBasename('.php'));
+  if ($r->isSubclassOf('Symfony\\Component\\Console\\Command\\Command') && !$r->isAbstract()) {
+    $cmd = $r->newInstance();
+    $app->add($cmd);
+  }
+}
+
+$finder = new Finder();
+$finder->files()->name('*.php')->in(__DIR__.'/../lib/Command')->depth(0);
+
+$ns = '\\Command\\';
 
 foreach ($finder as $file) {
   $r  = new \ReflectionClass($ns.$file->getBasename('.php'));
